b5a4a1c1d704847c65cbf3e09cfd8e1045b24c58
Subha Biswal <103360716+20SB@users.noreply.github.com>
2023-01-23 22:12:09 +0530

    Add files via upload

---
+++ Data Structures in JAVA/codes/Test4/Minimum_Count.java
@@ -0,0 +1,53 @@
+/*
+ * Given an integer N, find and return the count of minimum numbers, sum of whose squares is equal to N.
+That is, if N is 4, then we can represent it as : {1^2 + 1^2 + 1^2 + 1^2} and {2^2}. Output will be 1, as 1 is the minimum count of numbers required.
+Note : x^y represents x raise to the power y.
+Input Format :
+Integer N
+Output Format :
+Required minimum count
+Constraints :
+1 <= N <= 50
+ */
+package Test4;
+
+import java.util.Scanner;
+
+public class Minimum_Count {
+	public static int minCount(int n) {
+		if(n<=1) {
+			return n;
+		}
+		int ans =n;
+		for(int i=1;i<=n;i++) {
+			int sq = i*i;
+			if(sq>n) {
+				break;
+			}
+			ans = Math.min(ans, minCount(n-sq)+1);
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		System.out.println(minCount(num));
+	}
+}
+/*
+Sample Input 1 :
+12
+Sample Output 1 :
+3
+Sample Output 1 Explanation :
+12 can be represented as :
+1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1
+1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 1^1 + 2^2
+1^1 + 1^1 + 1^1 + 1^1 + 2^2 + 2^2
+2^2 + 2^2 + 2^2
+As we can see, the output should be 3.
+Sample Input 2 :
+9
+Sample Output 2 :
+1
+*/
